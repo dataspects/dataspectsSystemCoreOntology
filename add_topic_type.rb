615e173cc09d114ef74e20b899d342c07e6ab93f
Lex Sulzer <lex@sulzer.net>
2017-10-30 17:40:16 +0100

    WIP

---
+++ add_topic_type.rb
@@ -0,0 +1,47 @@
+# DSKMFCoreOntology$ REPOSITORY= NAME= DOMAIN= ruby add_type.rb
+
+name = ENV['NAME']
+extension = 'sRawContent'
+domain = ENV['DOMAIN']
+repository = ENV['REPOSITORY']
+
+files = {}
+
+files[name] = "{{ComponentType2
+|HasKeyword=
+}}[[Category:#{domain}Ontology]]"
+
+files["Template:#{domain} #{name}"] = "<includeonly>{{METADATA|Keywords={{{HasKeyword|}}}}}
+
+{{RegularProperty|PropertyName=HasSubjectType|PropertyValue=IBG #{name}}}
+
+{{DISPLAYTITLE:#{name} \"{{{HasTitle}}}\"}}
+{{#default_form:IBG#{name}}}</includeonly>
+<noinclude>[[Category:#{domain}Ontology]]</noinclude>"
+
+files["Form:#{name}"] = "{{{info|add title=Add #{name}|edit title=Edit #{name}|page name=C<unique number;random;10>}}}
+
+{{{for template|IBG #{name}}}}
+  {|class=formtable
+    !Title
+    |{{{field|HasTitle|input type=text}}}
+    |-
+    !Keywords
+    |{{{field|HasKeyword|input type=tokens|delimiter=;|values from property=HasKeyword}}}
+  |}
+{{{end template}}}
+
+{{ForTemplateMotivation}}
+
+{{ForTemplateIrregularProperty}}
+
+{{ForTemplateContext}}
+
+{{FormFooter}}
+[[Category:#{domain}Ontology]]"
+
+files.each do |name, content|
+  open("#{repository}/objects/#{domain} #{name}.#{extension}", 'w') do |f|
+    f.puts content
+  end
+end
